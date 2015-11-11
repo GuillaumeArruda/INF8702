db3e98304d7bd57e09e746e8fb06fc758a0eb17d
Guillaume Arruda <guillaume.arruda@gmail.com>
2015-11-11 10:30:48 -0500

    Construction depth buffer complete

--- TP4v3/FBO.cpp
+++ TP4v3/FBO.cpp
@@ -130,11 +130,17 @@ void CFBO::Init( int w, int h)
    glTexImage2D( m_Target, 0, GL_RGB, m_TextureW, m_TextureH, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL );
    glFramebufferTexture2DEXT( GL_FRAMEBUFFER_EXT, GL_COLOR_ATTACHMENT0_EXT, m_Target, m_Texture, 0 );
 
-   // TODO :
-   // Initialise la partir DEPTH du FBO
-   // ...
-   // ...
-   // ...
+   glGenTextures(1, &m_Profondeur);
+   glBindTexture(GL_TEXTURE_2D, m_Profondeur);
+   glTexImage2D(m_Target, 0, GL_DEPTH_COMPONENT16, m_TextureW, m_TextureH, 0, GL_DEPTH_COMPONENT, GL_FLOAT, 0);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_FUNC, GL_LEQUAL);
+   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_COMPARE_MODE, GL_COMPARE_R_TO_TEXTURE);
+
+   glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, m_Profondeur, 0);
 
    // Vérification des erreurs
    GLenum status = glCheckFramebufferStatusEXT( GL_FRAMEBUFFER_EXT );
